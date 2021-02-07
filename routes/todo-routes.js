fe640dc25cf4b486dbe87a954808f7dc85d9238f
micaelagoffe <micaela.goffe@gmail.com>
2021-02-07 11:30:13 +0100

    able to edit and remove todos

--- routes/todo-routes.js
+++ routes/todo-routes.js
@@ -16,6 +16,31 @@ router.post("/", async (req, res) => {
     const task = new ToDo ({name: req.body.content});
 
     await task.save();
+    
+    res.redirect("/");
+});
+
+
+router.get("/edit/:id", async (req, res) => {
+    const todo = await ToDo.findOne({_id: req.params.id});
+
+    res.render("edit.ejs", {todo: todo});
+});
+
+
+router.post("/edit", async (req, res) => {
+    await ToDo.updateOne(
+        {_id: req.body.id}, 
+        {name: req.body.name}
+    );
+
+    res.redirect("/");
+});
+
+
+router.get("/delete/:id", async (req, res) => {
+    await ToDo.deleteOne({_id:req.params.id});
+
     res.redirect("/");
 });
 
